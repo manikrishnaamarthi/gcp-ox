7688bffab01ab249db1b94c3c57e6492148eba14
Satyasai9191 <satyasaiseshagiri1919.com>
2024-10-15 17:56:16 +0530

    i have modified dashboard home page

---
+++ src/app/DashBoard/HomePage/SearchPage/page.tsx
@@ -0,0 +1,14 @@
+// page.tsx
+import React from 'react';
+import Search from './Search';
+
+const Page: React.FC = () => {
+  return (
+    <div>
+      
+      <Search />
+    </div>
+  );
+};
+
+export default Page;
