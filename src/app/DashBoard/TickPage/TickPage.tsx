9531115b22c08e806c6d9a4dd4911bfdf8abde05
Satyasai9191 <satyasaiseshagiri1919.com>
2024-11-12 19:40:34 +0530

    i made changes in users dashboard and connect to backednd

--- src/app/DashBoard/TickPage/TickPage.tsx
+++ src/app/DashBoard/TickPage/TickPage.tsx
@@ -1,11 +1,22 @@
+"use client"
 import React from 'react';
+import { IoChevronBackSharp } from 'react-icons/io5';
 import './TickPage.css';
-import { IoIosArrowBack } from "react-icons/io";
+import { useRouter } from 'next/navigation';
+
 
 const TickPage: React.FC = () => {
+    const router = useRouter();
+    const handleHomeClick = () => {
+        // Navigate to the /DashBoard/HomePage route
+        router.push('/DashBoard/HomePage');
+    };
+
     return (
         <div className="tick-container">
-            <div className="back-button"><IoIosArrowBack /></div>
+                <button className="back-button" onClick={() => router.back()}>
+                    <IoChevronBackSharp size={20} />
+                </button>
             <div className="tick-content">
                 
                 <div className="tick-icon">
@@ -13,7 +24,7 @@ const TickPage: React.FC = () => {
                 </div>
                 <h2>Your Services has been booked</h2>
                 <p>Oxivive team will call you for booking confirmation</p>
-                <button className="home-button">Back to Home</button>
+                <button className="home-button" onClick={handleHomeClick}>Back to Home</button>
             </div>
         </div>
     );
